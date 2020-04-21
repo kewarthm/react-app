7fd5b5a1967987fa30cd1c8a320c6c7897da521a
Matthew Kewarth <mattkewarth@hotmail.com>
2020-04-21 01:27:01 -0400

    Updates

---
+++ precache-manifest.39fcc8123b08ff7cd4b29f3d6d9d35ad.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a3be9d4db881c1323a9ff73193a8a791",
+    "url": "/react-app/index.html"
+  },
+  {
+    "revision": "f1ea182bbcf1a67a2803",
+    "url": "/react-app/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "c894e8a8e01d6a599ce8",
+    "url": "/react-app/static/js/2.07a05dc4.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-app/static/js/2.07a05dc4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f1ea182bbcf1a67a2803",
+    "url": "/react-app/static/js/main.e0f6ca94.chunk.js"
+  },
+  {
+    "revision": "1dc033653be254afbc4b",
+    "url": "/react-app/static/js/runtime-main.068afa05.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/react-app/static/media/logo.5d5d9eef.svg"
+  }
+]);
